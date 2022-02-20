78ec774f070fc1027d35e7449adb58cc323df7c7
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2022-02-20 18:17:16 +0700

    + Project Setup (Assets, Contants, Navigations)

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+//Screen
+import {Home, OrderDelivery, Restaurant} from "./src/screens";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Food Delivery App</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Food Delivery App</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Home from "./HomeScreen/Home";
+import OrderDelivery from "./OrderDeliveryScreen/OrderDelivery";
+import Restaurant from "./RestaurantScreen/Restaurant";
+
+export {
+    Home, 
+    OrderDelivery,
+    Restaurant
+}
